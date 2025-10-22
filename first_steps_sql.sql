6d1eb72146d2bc51e3805c10ac9eb0eccbe2e5de
Obed <obedco295@gmail.com>
2025-10-22 12:08:27 -0600

    Ejercicios basicos de como actualizar, seleccionar y eliminar datos de una tabla sql

--- first_steps_sql.sql
+++ first_steps_sql.sql
@@ -6,8 +6,39 @@ create table academics.students(
     , email NVARCHAR(100) not null UNIQUE
 )
 
+--- modificar taba
+alter table academics.students add age int null
+--- ejemplo para actualizar todos los regitros
+UPDATE academics.students
+set age = 19 --- sin where afecta a todos los registros
+
+-- si quiero actualizar el age en donde lastname = Castellanos
+UPDATE academics.students
+set age = 20
+WHERE lastname = 'Doe'
+
+
 
 SELECT * from academics.students
 
+---Ejemplo de insert
 INSERT into academics.students(firstname, lastname, idperson, email)
-VALUES('John', 'Doe', '1210291', 'john2.@gmail.com')
+VALUES('John', 'Doe', '121029', 'john23.@gmail.com')
+
+INSERT into academics.students(firstname, lastname, idperson, email, age)
+VALUES('Corina', 'Caballero', '113', 'corina23.@gmail.com', 18)
+---Ejemplo de actualizacion
+UPDATE academics.students
+SET firstname = 'Obed', lastname = 'Castellanos', email = 'obedc@gmail.com'
+WHERE id = 2
+
+--- EJemplo para eliminar un regristro
+delete from academics.students
+where id = 1
+
+SELECT id, email, firstname FROM academics.students WHERE id = 2
+
+
+--- selecionar todos los estudiantes menores a 20 anos
+SELECT * FROM academics.students
+WHERE age < 20
