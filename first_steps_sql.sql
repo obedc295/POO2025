311c07223bddaa3e93f670829ce7f5be1e920bf3
Obed <obedco295@gmail.com>
2025-10-22 15:29:34 -0600

    Ordenar archivo first_steps_sql.sql

--- first_steps_sql.sql
+++ first_steps_sql.sql
@@ -1,21 +1,10 @@
-create table academics.students(
-    id int IDENTITY(1,1) PRIMARY KEY
-    , firstname nvarchar(100) NOT NULL
-    , lastname nvarchar(100) NOT NULL
-    , idperson NVARCHAR(100) NOT NULL UNIQUE
-    , email NVARCHAR(100) not null UNIQUE
-)
 
-
-
-
-
+SELECT * from academics.students
 SELECT * from academics.careers
-
+SELECT * from academics.schools
 
 
 SELECT id, email, firstname FROM academics.students WHERE id = 2
-
 
 --- selecionar todos los estudiantes menores a 20 anos
 SELECT * FROM academics.students_careers
